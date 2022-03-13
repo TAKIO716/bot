7dc88f8dfeaf7b279a596f805f46c52335e7f584
Squateditz <100348167+Squateditz@users.noreply.github.com>
2022-03-13 12:45:35 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 *Hayo ngapain mau pasang ya?*
 *Nih w kasi, kasian amat lu tekan y itu link*
-*https://github.com/ItzLighting9332/nayla*
+*beli lah ngen enak banget tinggal ambil*
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*NIH KASIAN LU*', 'status@broadcast')
