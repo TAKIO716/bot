539d0204354ab76bc10f1297cdc2471d5d07258e
Squateditz <100348167+Squateditz@users.noreply.github.com>
2022-03-13 12:33:06 +0700

    Update buygift.js

--- plugins/buygift.js
+++ plugins/buygift.js
@@ -7,13 +7,14 @@ let handler = async m => m.reply(`
 │sepuasnya! Hingga masa berlaku kodegiftnya habis!
 │
 │ > Harga :
-│• 5K / Minggu (7 Hari)
-│• 15K / Bulan (4 Minggu)
-│• 30K / VIP (Permanen)
-│
+│• 10K / Minggu (7 Hari)
+│• 25K / Bulan (4 Minggu)
+│• 200K / tahun (Permanen)
+│• 40k pertama 30k/bulan prem and jadi bot
+│• 60k pertama 40k/bulan jadibot and owner
 │ > Pembayaran :
-│• Dana : 628818770766
-│• Gopay : 628818770766
+│• Dana : 629616061778
+│• Tri : 6289616061778
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['buygift']
