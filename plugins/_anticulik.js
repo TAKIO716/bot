8c3f3b7382bc02fb4652ca5cc86d7dcba8f12f5d
Squateditz <100348167+Squateditz@users.noreply.github.com>
2022-03-13 12:28:57 +0700

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -2,16 +2,15 @@ let handler = m => handler.all = async function (m, { isAdmin, isBotAdmin }) {
   if (m.mtype == 'groupInviteMessage') {
 idnya = await this.reply(m.chat, `「 Donasi • Pulsa 」
 
-• *Gopay:* 6281215199447
-• *Telkomsel:* 6281215199447
-• *Indosat:* 6285642337380
+• *Dana:* 6289616061778
+• *Tri:* 6289616061778
 
 Donasi semampunya untuk mendukung bot ini tetap hidup untuk kalian^_^
-Kalau gak mampu yang di atas bisa juga *follow ig* https://instagram.com/caliph91_
+Kalau gak mampu yang di atas bisa juga *SUBSCRIBE* https://youtube.com/channel/UCIzklEsl8mjhm3R-fTniucA
 
 Kontak pemilik saya⬇️⬇️
 `, m)
-this.sendContact(m.chat, '6281215199447', 'Pemilik saya', idnya)
+this.sendContact(m.chat, '6289616061778', 'Pemilik saya', idnya)
   }
   return true
 }
